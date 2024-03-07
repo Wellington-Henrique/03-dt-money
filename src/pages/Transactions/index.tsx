9842bebff57e6b52348ffff321f41adabb5a8de9
Wellington Henrique <wellingtonhlc@gmail.com>
2024-03-07 10:09:21 -0300

    Conexão com a API: Realizando requisição HTTP

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transaction {
+    id: number
+    description: string
+    type: "income" | 'outcome'
+    price: number
+    category: string
+    createdAt: string
+}
+
 export function Transactions() {
+    const [transactions, setTransactions] = useState<Transaction[]>([])
+
+    useEffect(() => {
+        loadTransactions();
+    }, [])
+
+    async function loadTransactions() {
+        const response = await fetch("http://localhost:3000/transactions")
+        const data = await response.json()
+       
+        setTransactions(data ?? [])
+    }
+
     return (
         <div>
             <Header/>
@@ -13,27 +36,18 @@ export function Transactions() {
                 <SearchForm/>
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td width='50%'>Desenvolvimento de site</td>
+                        {transactions.length &&
+                        transactions.map(transaction => 
+                        <tr key={transaction.id}>
+                            <td width='50%'>{transaction.description}</td>
                             <td>
-                                <PriceHighLight variant='income'>
-                                    R$ 12.000,00
+                                <PriceHighLight variant={transaction.type}>
+                                    {transaction.price}
                                 </PriceHighLight>
                             </td>
-
-                            <td>Venda</td>
-                            <td>13/04/2022</td>
-                        </tr>
-                        <tr>
-                            <td width='50%'>Hamburguer</td>
-                            <td>
-                                <PriceHighLight variant='outcome'>
-                                    - R$ 59,00
-                                </PriceHighLight>
-                            </td>
-                            <td>Alimentação</td>
-                            <td>10/04/2022</td>
-                        </tr>
+                            <td>{transaction.category}</td>
+                            <td>{transaction.createdAt}</td>
+                        </tr>)}
                     </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
