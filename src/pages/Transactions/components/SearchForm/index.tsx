5281fc383d0fa9b30151855eb76192431f7edec0
Wellington Henrique <wellingtonhlc@gmail.com>
2024-03-07 11:24:15 -0300

    Conexão com a API: Aplicando React Hook Form

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,42 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from 'zod'
+
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
 
+const searchFormSchema = z.object({
+    query: z.string()
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
 export function SearchForm() {
+    const { 
+        register, 
+        handleSubmit, 
+        formState: {
+            isSubmitting
+        }
+    } = useForm<SearchFormInputs>({
+        resolver: zodResolver(searchFormSchema)
+    })
+
+    async function handleSearchTransactions(data: SearchFormInputs) {
+        await new Promise(resolve => setTimeout(resolve, 2000))
+        
+        console.log(data)
+    }
+
     return (
-        <SearchFormContainer>
-            <input type="text" placeholder="🔍 Busque por transações..." />
+        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+            <input 
+                type="text" 
+                placeholder="🔍 Busque por transações..." 
+                {...register('query')}
+            />
             
-            <button type="submit">
+            <button type="submit" disabled={isSubmitting}>
                 <MagnifyingGlass size={20}/>
                 Buscar
             </button>
